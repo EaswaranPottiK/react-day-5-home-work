848130d0cda33c3328b9e17c182e13f01fe1d666
Easwaran <easwaranpottiak@gmail.com>
2023-12-25 15:15:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import data from './data'
 
 function App() {
+
+  const [count,setCount] = useState(1);
+  function updateCount(count){
+    if (count<0){
+      setCount(0)
+      alert('Ayyo ! you know that you wrote negative input 😵')
+    }
+    else if (count >8){
+      setCount(0)
+      alert('Ayyo ! you are demanding very much paragraph in one go, kindly take little litlle 😀')
+    }
+    else {
+      setCount(count)
+    }
+  }
+
+
+  const [text,setText] = useState([])
+  function generate(e){
+    e.preventDefault();
+    setText(data.slice(0,count))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor:'#FFE6D6', textAlign:'center'}}>
+      <h1 style={{color:'#B0360B'}}>TIRED OF BORING LOREM IPSUM?</h1>
+      <div style={{display:'flex',gap:'10px',alignItems:'center',justifyContent:'center'}}>
+        <p>Paragraphs:</p>
+        <input type='number' onChange={(e)=>{updateCount(e.target.value)}} style={{border:'none', width:'4vw',height:'4vh', borderRadius:'4px'}}></input>
+        <button style={{color:'white',backgroundColor:'#F5873D', padding:'6px 12px', border:'none',borderRadius:'4px'}} onClick={(e)=>generate(e)}>GENERATE</button>
+      </div>
+      <div style={{color:'#B3430A', width:'50%', margin:'auto'}}>
+        {
+          text.map((item,itemNo)=>{
+            return <p key={itemNo}>{item}</p>
+          })
+        }
+      </div>
     </div>
   );
 }
